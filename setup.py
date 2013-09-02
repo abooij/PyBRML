1043c0b3e53f0db978692254efc2d7a1cdb3477b
Auke Booij <auke@tulcod.com>
2013-09-02 13:33:49 +0200

    Add project files for a python3 project

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='brml',
+      version='0',
+      description='Python version of Bayesian Reasoning and Machine Learning library',
+      packages=['brml'],
+      install_requires=['numpy']
+)
